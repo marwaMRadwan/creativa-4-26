e9122a8a099f9180a0b62e0ea9a650d5df95ed09
marwaMRadwan <marwaradwan872@hotmail.com>
2026-05-04 16:03:53 +0300

    s11

---
+++ s11/js/script.js
@@ -0,0 +1,111 @@
+// console.log("test")
+// variables
+// any - void
+// x = 5
+// x= "abc"
+// x = true
+// x = []
+// var , let, const
+
+// console.log(x)
+// console.log(z)
+
+// var x 
+// x = 3
+
+// var x = "bhdbhj"
+// const PI = 3.14
+// let z 
+// z= 3
+// let z = 5
+
+
+// let val = 6
+// console.log(typeof val)
+// val = "abc"
+// console.log(typeof val)
+
+// opertions
+// mathmatical + - / * % **
+// console.log(5 ** 2)
+// console.log( 5 % 2)
+
+
+// (5 + 2) * 3 / 6 - 7 * 4
+
+// 5 + 6 / 6 - 28
+// 5 + 1 - 28
+// 6 - 28
+// -22
+
+// assignment operators
+// = += -= /= %= *=
+
+// comp 
+// == === != !== > < >= <= 
+// 5==="5"
+
+// logical
+// && || !
+
+// unry
+// & ! |
+
+//inc and dec
+// ++ -- 
+
+// let x = 5
+// y = x++
+// y = ++x
+
+// ternery opertator
+// ?
+// let x = 3
+// x == 5 ? console.log("yes") : 
+
+// let z = +prompt("get data")
+// if(typeof z == "number"){
+//     console.log((Number(z)) + 6)
+// }
+
+// console.log( z + 3)
+
+// let grade = 50
+
+// if(grade > 70 ){
+//     console.log("test")
+//     console.log("abc")
+// }
+// else {
+
+// }
+
+// if(cond1){}
+// else if(cond1){}
+// else if(cond3){}
+// else{}
+
+
+// if(x>90){}
+// else if(x>80){}
+// else if(x>70){}
+
+
+// let w = prompt("enter w in kg")
+// let h = prompt("enter h in m")
+// let bmi = w / (h ** 2)
+// let result
+// if(bmi < 18.5) result = ""
+// else if(bmi < 24.9) result = ""
+// else result=""
+// console.log(result)
+
+let weekday = 1
+switch(weekday){
+    case 1: 
+        console.log(1);
+        console.log("test")
+        break
+    case 2: console.log(2);break
+    default: console.log("else")
+}
